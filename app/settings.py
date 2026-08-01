9c299aa6dcd063c019f0170d50dbd541ccdfd9c6
lampapps <jamesoferry@gmail.com>
2026-08-01 17:39:03 -0400

    Update README and settings version to 0.10.1; enhance feature descriptions and clarify agent registration process

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 
-VERSION = "v0.10.0"
+VERSION = "v0.10.1"
 
 # --- Environment Configuration ---
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
